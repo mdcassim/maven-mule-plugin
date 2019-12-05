9f3b85d5346e6a434fc0a0c3d1128fdb38aa6bd3
Mohamed Cassim <mdcassimsait@gmail.com>
2019-12-05 12:07:31 +0530

    Update Demo.java

--- src/main/java/org/mule/tools/maven/plugin/Demo.java
+++ src/main/java/org/mule/tools/maven/plugin/Demo.java
@@ -3,6 +3,6 @@ public class Demo {
 
     public static void main(String args[]) 
     {
-        System.out.println(" Java Hungry ");       
+        System.out.println(" Mule4 Project Deploying through Jenkins pipeline ");       
     }
 }
